0c234b9e11fa85f36a160f7c7b072aa51ae6bc8b
Tejas Wanjari <tjs-w@github.com>
2025-09-23 23:40:09 -0700

    fix: update husky deprecated script
    - Remove deprecated husky script that will fail in v10.0.0
    - This maintains compatibility with newer husky versions

--- .husky/_/husky.sh
+++ .husky/_/husky.sh
@@ -1,31 +1,9 @@
+echo "husky - DEPRECATED
+
+Please remove the following two lines from $0:
+
 #!/usr/bin/env sh
-if [ -z "$husky_skip_init" ]; then
-    debug() {
-        if [ "$HUSKY_DEBUG" = "1" ]; then
-            echo "husky (debug) - $1"
-        fi
-    }
+. \"\$(dirname -- \"\$0\")/_/husky.sh\"
 
-    readonly hook_name="$(basename -- "$0")"
-    debug "starting $hook_name..."
-
-    if [ "$HUSKY" = "0" ]; then
-        debug "HUSKY env variable is set to 0, skipping hook"
-        exit 0
-    fi
-
-    if [ -f ~/.huskyrc ]; then
-        debug "sourcing ~/.huskyrc"
-        . ~/.huskyrc
-    fi
-
-    readonly husky_root="$(dirname -- "$(dirname -- "$0")")"
-    readonly hook_path="$husky_root/$hook_name"
-
-    if [ -f "$hook_path" ]; then
-        debug "running $hook_path"
-        . "$hook_path"
-    else
-        debug "$hook_path not found, skipping"
-    fi
-fi
+They WILL FAIL in v10.0.0
+"
